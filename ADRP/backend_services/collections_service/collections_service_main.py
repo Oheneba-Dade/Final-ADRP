d34acfd93cf5967f505afea8d577f9efa495c132
SKBlankson <114783019+SKBlankson@users.noreply.github.com>
2025-02-25 18:13:47 +0000

    Service and view folder creation
    - Created folders for services
    - Created folders for views

---
+++ ADRP/backend_services/collections_service/collections_service_main.py
@@ -0,0 +1,25 @@
+class CollectionsService:
+    """Service for managing collections on the ADRP"""
+
+
+    def createCollection(self):
+        # define business logic here
+        # any queries get called in from the collections_service_repository
+        return
+
+
+    def deleteCollection(self):
+        return
+
+
+
+    def updateCollection(self):
+        return
+
+
+
+    def getCollection(self):
+        return
+
+
+
