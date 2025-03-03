bb6d4eed24c4d3e35c37b55e54272a5c48dc4397
Reynolds Okyere <boakyeokyere96@gmail.com>
2025-03-03 19:00:09 +0000

    fix: footer links

--- ashesi_drp_frontend/src/components/FooterLink.js
+++ ashesi_drp_frontend/src/components/FooterLink.js
@@ -3,12 +3,12 @@ import { LuArrowRight } from "react-icons/lu";
 
 const FooterLink = ({ title, link }) => {
 	return (
-		<div className="bg-ashesi-gray w-full mb-2 p-2 hover:cursor-pointer hover:bg-gray-400">
-			<Link href={link} className="text-white px-2">
-				{title}
-				<LuArrowRight className="inline-block float-right" />
-			</Link>
-		</div>
+		<Link href={link} className="block">
+		  <div className="bg-ashesi-gray w-full mb-2 p-2 hover:cursor-pointer hover:bg-gray-400 flex justify-between items-center">
+		    <span className="text-white px-2">{title}</span>
+		    <LuArrowRight className="text-white" />
+		  </div>
+		</Link>
 	);
 };
 
