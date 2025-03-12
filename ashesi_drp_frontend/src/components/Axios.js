acb0c54c9ea7eb8062bc2b4b9bba2426f18a1b7c
Yasmin A.G. Kamal-Deen <yasmin@Yasmins-MacBook-Pro.local>
2025-03-12 19:11:36 +0000

    adding axios and changing requirements

---
+++ ashesi_drp_frontend/src/components/Axios.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const baseUrl = 'http://localhost:8000/adrp/'
+const AxiosInstance = axios.create({
+    baseURL: baseUrl,
+    timeout: 3000,
+    headers: {
+        "Content-Type":"application/json",
+        "Accept" : "application/json"
+    }
+})
+
+
+export default AxiosInstance
