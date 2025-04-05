684a7e1fd72f2a1157cc09117f96786b33de03bd
Oheneba Kwaku Addo Dade <ohenebadade@gmail.com>
2025-04-05 12:03:47 +0000

    fix: collections page filter and collections merge

--- ashesi_drp_frontend/src/components/CollectionsContainer.js
+++ ashesi_drp_frontend/src/components/CollectionsContainer.js
@@ -30,7 +30,6 @@ export default function CollectionsContainer({ initialCollections, filterOn=true
         try {
             const response = await fetch(url);
             const data = await response.json();
-            // console.log(response);
             setCollections(data);
         } catch (error) {
             console.error("Error fetching collections:", error);
@@ -39,14 +38,13 @@ export default function CollectionsContainer({ initialCollections, filterOn=true
         }
     };
 
-
     const handleFilterResults = (filteredData) => {
         // Check if the data has the expected structure
         if (filteredData && filteredData.results !== undefined) {
             setCollections(filteredData);
         } else {
             // Handle the case where the response doesn't have the expected structure
-            setCollections({ results: [], next: null, previous: null });
+            setCollections({ results: [], next: null, previous: null, count: 0 });
         }
     };
 
@@ -74,106 +72,141 @@ export default function CollectionsContainer({ initialCollections, filterOn=true
         }
     };
 
-
     return (
-        <div className="mb-14 mx-auto w-full grid grid-cols-1 md:grid-cols-6 gap-20 px-16">
-
-            {
-                filterOn && (
-                    <div className="md:col-span-2 mt-10">
+        <div className="mb-14 w-full px-4 md:px-8 lg:px-16">
+            <div className="flex flex-col md:flex-row md:gap-8 lg:gap-20">
+                {filterOn && (
+                    <div className="w-full md:w-auto md:min-w-[320px] mb-8 md:mb-0">
                         <Filter
                             onFilterResults={handleFilterResults}
                             onResetFilter={handleResetFilter}
                             setLoading={setLoading}
                         />
                     </div>
-                )
-            }
+                )}
 
-            <div className={filterOn ? "md:col-span-4 max-w-3xl" : "md:col-span-6 max-w-3xl"}>
-                {loading ? (
-                    <div className="py-10 px-4 border-b">
-                        <div className="py-10 px-4 border-b ">
-                            <Skeleton width={550} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                            <Skeleton height={100} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                            <div className="mt-4 flex flex-wrap justify-between text-sm text-ashesi-gray">
-                                <Skeleton width={210} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                                <Skeleton width={200} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                            </div>
+                <div className={`w-full ${filterOn ? "md:flex-1" : "max-w-3xl mx-auto"}`}>
+                    {loading ? (
+                        <LoadingSkeleton />
+                    ) : (
+                        <>
+                            {collections.results && collections.count > 0 ? (
+                                <>
+                                    <ul>
+                                        {collections.results.map((collection) => (
+                                            <CollectionItem
+                                                key={collection.id}
+                                                collection_id={collection.id}
+                                                title={collection.title}
+                                                authors={collection.authors}
+                                                abstract={collection.abstract}
+                                                date_of_publication={collection.date_of_publication}
+                                                doi_link={collection.doi_link}
+                                                keywords={collection.keywords}
+                                            />
+                                        ))}
+                                    </ul>
 
-                            <div className="mt-1 flex flex-wrap justify-between text-sm text-ashesi-gray">
-                                <Skeleton width={200} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                                <Skeleton width={250} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                            </div>
-                        </div>
-
-                        {/* Second */}
-                        <div className="py-10 px-4 border-b ">
-                            <Skeleton width={550} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                            <Skeleton height={100} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                            <div className="mt-4 flex flex-wrap justify-between text-sm text-ashesi-gray">
-                                <Skeleton width={210} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                                <Skeleton width={200} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                            </div>
-
-                            <div className="mt-1 flex flex-wrap justify-between text-sm text-ashesi-gray">
-                                <Skeleton width={200} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                                <Skeleton width={250} height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
-                            </div>
-                        </div>
-                    </div>
-                ) : (
-                    <>
-                        {collections.results && collections.count > 0 ? (
-                            <>
-                                <ul>
-                                    {collections.results.map((collection) => (
-                                        <CollectionItem
-                                            key={collection.id}
-                                            collection_id={collection.id}
-                                            title={collection.title}
-                                            authors={collection.authors}
-                                            abstract={collection.abstract}
-                                            date_of_publication={collection.date_of_publication}
-                                            doi_link={collection.doi_link}
-                                            keywords={collection.keywords}
-                                        />
-                                    ))}
-                                </ul>
-
-                                {
-                                    filterOn && (
+                                    {filterOn && (
                                         <CollectionsPagination
                                             nextUrl={collections.next}
                                             previousUrl={collections.previous}
                                             onPageChange={fetchPage}
                                         />
-                                    )
-                                }
-                            </>
-                        ) : (
-                            <>
-                            <div className="flex flex-col items-center justify-center text-white px-6">
-                                {/* Lottie Animation (Only Render if JSON is Loaded) */}
-                                {animationData && (
-                                    <motion.div
-                                        initial={{ opacity: 0, scale: 0.8 }}
-                                        animate={{ opacity: 1, scale: 1 }}
-                                        transition={{ duration: 0.6, ease: "easeOut" }}
-                                        className="w-100 h-100 lg:w-[300px] lg:h-[300px]"
-                                    >
-                                        <Lottie animationData={animationData} loop={true} />
-                                    </motion.div>
-                                )}
-                                <div className="flex flex-col items-center justify-center py-20">
-                                    <p className="text-xl text-gray-600 mb-4">Sorry, no results found</p>
-                                    <p className="text-sm text-gray-500">Try adjusting your search criteria</p>
-                                </div>
-                            </div>
-                            </>
-                        )}
-                    </>
-                )}
+                                    )}
+                                </>
+                            ) : (
+                                <NoResultsFound animationData={animationData} />
+                            )}
+                        </>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function LoadingSkeleton() {
+    return (
+        <div className="py-10 px-4 border-b">
+            <div className="py-10 px-4 border-b">
+                <Skeleton width="100%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                <Skeleton height={100} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                <div className="mt-4 flex flex-wrap justify-between text-sm text-ashesi-gray">
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                </div>
+
+                <div className="mt-1 flex flex-wrap justify-between text-sm text-ashesi-gray">
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                    <Skeleton width="50%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                </div>
+            </div>
+
+            {/* Second skeleton */}
+            <div className="py-10 px-4 border-b">
+                <Skeleton width="100%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                <Skeleton height={100} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                <div className="mt-4 flex flex-wrap justify-between text-sm text-ashesi-gray">
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                </div>
+
+                <div className="mt-1 flex flex-wrap justify-between text-sm text-ashesi-gray">
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                    <Skeleton width="50%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                </div>
+            </div>
+
+            <div className="py-10 px-4 border-b">
+                <Skeleton width="100%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                <Skeleton height={100} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                <div className="mt-4 flex flex-wrap justify-between text-sm text-ashesi-gray">
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                </div>
+
+                <div className="mt-1 flex flex-wrap justify-between text-sm text-ashesi-gray">
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                    <Skeleton width="50%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                </div>
+            </div>
+
+
+            <div className="py-10 px-4 border-b">
+                <Skeleton width="100%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                <Skeleton height={100} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                <div className="mt-4 flex flex-wrap justify-between text-sm text-ashesi-gray">
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                </div>
+
+                <div className="mt-1 flex flex-wrap justify-between text-sm text-ashesi-gray">
+                    <Skeleton width="45%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                    <Skeleton width="50%" height={30} baseColor="#dce3e8" highlightColor="#f0f4f8"/>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+function NoResultsFound({ animationData }) {
+    return (
+        <div className="flex flex-col items-center justify-center text-white px-6">
+            {animationData && (
+                <motion.div
+                    initial={{ opacity: 0, scale: 0.8 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 0.6, ease: "easeOut" }}
+                    className="w-100 h-100 lg:w-[300px] lg:h-[300px]"
+                >
+                    <Lottie animationData={animationData} loop={true} />
+                </motion.div>
+            )}
+            <div className="flex flex-col items-center justify-center py-20">
+                <p className="text-x33l text-gray-600 mb-4">Sorry, no results found</p>
+                <p className="text-sm text-gray-500">Try adjusting your search criteria</p>
             </div>
         </div>
     );
--- ashesi_drp_frontend/src/components/Filter.js
+++ ashesi_drp_frontend/src/components/Filter.js
@@ -1,11 +1,233 @@
+// "use client";
+// import CustomButton from "@/components/CustomButton";
+// import KeywordInput from "@/components/KeywordInput";
+// import {useEffect, useState} from "react";
+// import {BASE_URL} from "@/utils/constants";
+// import {useSearchParams} from "next/navigation";
+//
+//
+//
+// export default function Filter({ onFilterResults, onResetFilter, setLoading }) {
+// 	const d = new Date();
+// 	const [fromYear, setFromYear] = useState("1900");
+// 	const [toYear, setToYear] = useState(new Date().getFullYear().toString());
+// 	const [keywords, setKeywords] = useState([]);
+// 	const searchParams = useSearchParams();
+// 	const initialTitle = searchParams.get("title") || "";
+//
+// 	useEffect(() => {
+// 		if (searchParams.toString()) {
+// 			handleSearch();
+// 		}
+// 	}, [searchParams]);
+//
+//
+//
+//
+// 	const handleFromChange = (e) => {
+// 		let value = e.target.value.replace(/\D/g, "");
+// 		if (value.length > 4) value = value.slice(0, 4);
+//
+// 		let numValue = parseInt(value, 10);
+// 		if (isNaN(numValue) || numValue < 1900) numValue = 1900;
+// 		if (numValue > toYear) numValue = toYear;
+//
+// 		setFromYear(numValue.toString());
+// 	};
+//
+// 	const handleToChange = (e) => {
+// 		let value = e.target.value.replace(/\D/g, "");
+// 		if (value.length > 4) value = value.slice(0, 4);
+//
+// 		let numValue = parseInt(value, 10);
+// 		if (isNaN(numValue) || numValue < fromYear) numValue = fromYear;
+// 		if (numValue > d.getFullYear()) numValue = d.getFullYear();
+//
+// 		setToYear(numValue.toString());
+// 	};
+//
+//
+//
+// 	//TODO: Add authorization headers to include access tokens
+// 	const sendFilterRequest = async (baseURL, queryParams, callback) => {
+// 		const url = new URL(`${baseURL}/collections`);
+//
+// 		// Add each query parameter to the URL (keep this part the same)
+// 		Object.keys(queryParams).forEach(key => {
+// 			if (queryParams[key]) {
+// 				if (key === "keywords" && Array.isArray(queryParams[key]) && queryParams[key].length > 0) {
+// 					url.searchParams.append(key, queryParams[key].join(','));
+// 				} else {
+// 					url.searchParams.append(key, queryParams[key]);
+// 				}
+// 			}
+// 		});
+//
+// 		try {
+// 			const response = await fetch(url, {
+// 				method: "GET",
+// 				headers: {
+// 					'Content-Type': 'application/json'
+// 				}
+// 			});
+//
+// 			if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+// 			const result = await response.json();
+//
+// 			// Call the callback with the result
+// 			if (callback) callback(result);
+//
+// 			return result;
+// 		} catch (error) {
+// 			console.error("Error sending request:", error);
+// 			throw error;
+// 		}
+// 	};
+//
+// 	const handleSearch = async (e) => {
+// 		if (e) e.preventDefault(); // Prevent default only if triggered by an event
+// 		setLoading(true);
+//
+// 		let collectionName = document.getElementsByName("collection-name")[0].value;
+// 		let author = document.getElementsByName("author")[0].value;
+//
+// 		const queryParams = {
+// 			title: collectionName,
+// 			keywords: keywords,
+// 			author: author,
+// 			published_after: fromYear,
+// 			published_before: toYear,
+// 		};
+//
+// 		try {
+// 			await sendFilterRequest(BASE_URL, queryParams, onFilterResults);
+// 		} catch (error) {
+// 			console.error("Error sending GET request:", error);
+// 		} finally {
+// 			setLoading(false);
+// 		}
+// 	};
+//
+//
+// 	const handleReset = (e) => {
+// 		e.preventDefault();
+// 		// Reset form fields
+// 		document.getElementsByName("collection-name")[0].value = "";
+// 		document.getElementsByName("author")[0].value = "";
+// 		setKeywords([]);
+// 		setFromYear("1900");
+// 		setToYear(new Date().getFullYear().toString());
+//
+// 		if (onResetFilter) {
+// 			onResetFilter();
+// 		}
+// 	};
+//
+//
+// 	return (
+// 		<form
+// 			className="flex flex-col items-center bg-gray-50 py-8 rounded-lg justify-center mb-8 p-8 ml-12 w-96 shadow-md"
+// 		>
+// 			<div className="mb-8">
+// 				<label
+// 					htmlFor="search-collection"
+// 					className="mb-4 text-gray-600"
+// 				>
+// 					Search by collection name{" "}
+// 				</label>
+// 				<input
+// 					type="text"
+// 					className="w-80 outline-ashesi-red border border-ashesi-red rounded-md py-1 px-4"
+// 					name="collection-name"
+// 					defaultValue={initialTitle}  // Allows user edits without React tracking state
+// 				/>
+// 			</div>
+//
+// 			<div className="mb-8">
+// 				<label
+// 					htmlFor="search-collection"
+// 					className="mb-4 text-gray-600"
+// 				>
+// 					Search by author{" "}
+// 				</label>
+// 				<input
+// 					type="text"
+// 					className="w-80 outline-ashesi-red border border-ashesi-red rounded-md py-1 px-4"
+// 					name="author"
+// 				/>
+// 			</div>
+//
+// 			<div className="mb-8">
+// 				<label
+// 					htmlFor="search-collection"
+// 					className="mb-4 text-gray-600"
+// 				>
+// 					Search by keyword
+// 				</label>
+// 				<KeywordInput onKeywordsChange={setKeywords} />
+// 			</div>
+//
+// 			<span className="text-center text-gray-600 mb-4">
+// 				PUBLISHED FROM
+// 			</span>
+// 			<div className="flex gap-8 w-full items-center justify-center mb-8">
+// 				<input
+// 					type="number"
+// 					className="w-20 border border-ashesi-red rounded-md p-1 pl-4 focus:outline-ashesi-red"
+// 					placeholder="YYYY"
+// 					value={fromYear}
+// 					onChange={handleFromChange}
+// 					onKeyDown={(e) => {
+// 						if (["e", "E", "+", "-", "."].includes(e.key))
+// 							e.preventDefault();
+// 					}}
+// 					onBlur={() => {
+// 						if (!fromYear) setFromYear("1900");
+// 					}}
+// 				/>
+//
+// 				<span className="text-gray-600">TO</span>
+// 				<input
+// 					type="number"
+// 					className="w-20 border border-ashesi-red rounded-md p-1 pl-4 focus:outline-ashesi-red"
+// 					placeholder="YYYY"
+// 					value={toYear}
+// 					onChange={handleToChange}
+// 					onKeyDown={(e) => {
+// 						if (["e", "E", "+", "-", "."].includes(e.key))
+// 							e.preventDefault();
+// 					}}
+// 					onBlur={() => {
+// 						if (!toYear) setToYear("2100");
+// 					}}
+// 				/>
+// 			</div>
+// 			<div className="mb-4 flex justify-between w-full px-8">
+// 				<CustomButton
+// 					text="GO"
+// 					width="w-28"
+// 					height = "h-8"
+// 					onClick={handleSearch}
+// 					type="button"
+// 				/>
+// 				<CustomButton
+// 					text="RESET"
+// 					width="w-28"
+// 					height = "h-8"
+// 					onClick={handleReset}
+// 					type="button"
+// 				/>
+// 			</div>
+// 		</form>
+// 	);
+// }
+
 "use client";
 import CustomButton from "@/components/CustomButton";
 import KeywordInput from "@/components/KeywordInput";
 import {useEffect, useState} from "react";
 import {BASE_URL} from "@/utils/constants";
 import {useSearchParams} from "next/navigation";
-
-
 
 export default function Filter({ onFilterResults, onResetFilter, setLoading }) {
 	const d = new Date();
@@ -20,9 +242,6 @@ export default function Filter({ onFilterResults, onResetFilter, setLoading }) {
 			handleSearch();
 		}
 	}, [searchParams]);
-
-
-
 
 	const handleFromChange = (e) => {
 		let value = e.target.value.replace(/\D/g, "");
@@ -46,13 +265,11 @@ export default function Filter({ onFilterResults, onResetFilter, setLoading }) {
 		setToYear(numValue.toString());
 	};
 
-
-
 	//TODO: Add authorization headers to include access tokens
 	const sendFilterRequest = async (baseURL, queryParams, callback) => {
 		const url = new URL(`${baseURL}/collections`);
 
-		// Add each query parameter to the URL (keep this part the same)
+		// Add each query parameter to the URL
 		Object.keys(queryParams).forEach(key => {
 			if (queryParams[key]) {
 				if (key === "keywords" && Array.isArray(queryParams[key]) && queryParams[key].length > 0) {
@@ -108,7 +325,6 @@ export default function Filter({ onFilterResults, onResetFilter, setLoading }) {
 		}
 	};
 
-
 	const handleReset = (e) => {
 		e.preventDefault();
 		// Reset form fields
@@ -123,44 +339,43 @@ export default function Filter({ onFilterResults, onResetFilter, setLoading }) {
 		}
 	};
 
-
-	return ( 
+	return (
 		<form
-			className="flex flex-col items-center bg-gray-50 py-8 rounded-lg justify-center mb-8 p-8 ml-12 w-96 shadow-md"
+			className="flex flex-col items-center bg-gray-50 py-8 rounded-lg justify-center mb-8 p-8 md:ml-12 w-full md:w-96 shadow-md"
 		>
-			<div className="mb-8">
+			<div className="mb-8 w-full">
 				<label
 					htmlFor="search-collection"
-					className="mb-4 text-gray-600"
+					className="mb-4 text-gray-600 block"
 				>
 					Search by collection name{" "}
 				</label>
 				<input
 					type="text"
-					className="w-80 outline-ashesi-red border border-ashesi-red rounded-md py-1 px-4"
+					className="w-full outline-ashesi-red border border-ashesi-red rounded-md py-1 px-4"
 					name="collection-name"
 					defaultValue={initialTitle}  // Allows user edits without React tracking state
 				/>
 			</div>
 
-			<div className="mb-8">
+			<div className="mb-8 w-full">
 				<label
 					htmlFor="search-collection"
-					className="mb-4 text-gray-600"
+					className="mb-4 text-gray-600 block"
 				>
 					Search by author{" "}
 				</label>
 				<input
 					type="text"
-					className="w-80 outline-ashesi-red border border-ashesi-red rounded-md py-1 px-4"
+					className="w-full outline-ashesi-red border border-ashesi-red rounded-md py-1 px-4"
 					name="author"
 				/>
 			</div>
 
-			<div className="mb-8">
+			<div className="mb-8 w-full">
 				<label
 					htmlFor="search-collection"
-					className="mb-4 text-gray-600"
+					className="mb-4 text-gray-600 block"
 				>
 					Search by keyword
 				</label>
@@ -170,7 +385,7 @@ export default function Filter({ onFilterResults, onResetFilter, setLoading }) {
 			<span className="text-center text-gray-600 mb-4">
 				PUBLISHED FROM
 			</span>
-			<div className="flex gap-8 w-full items-center justify-center mb-8">
+			<div className="flex gap-4 md:gap-8 w-full items-center justify-center mb-8">
 				<input
 					type="number"
 					className="w-20 border border-ashesi-red rounded-md p-1 pl-4 focus:outline-ashesi-red"
@@ -202,17 +417,17 @@ export default function Filter({ onFilterResults, onResetFilter, setLoading }) {
 					}}
 				/>
 			</div>
-			<div className="mb-4 flex justify-between w-full px-8">
+			<div className="mb-4 flex justify-between w-full px-2 md:px-8">
 				<CustomButton
 					text="GO"
-					width="w-28"
+					width="w-24 md:w-28"
 					height = "h-8"
 					onClick={handleSearch}
 					type="button"
 				/>
 				<CustomButton
 					text="RESET"
-					width="w-28"
+					width="w-24 md:w-28"
 					height = "h-8"
 					onClick={handleReset}
 					type="button"
