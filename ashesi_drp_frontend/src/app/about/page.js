16f52b8a52365c12b0dc5f576e37ba27e6b90137
Oheneba Kwaku Addo Dade <ohenebadade@gmail.com>
2025-04-11 19:48:12 +0000

    feat: use ISR for stats on about page

--- ashesi_drp_frontend/src/app/about/page.js
+++ ashesi_drp_frontend/src/app/about/page.js
@@ -4,6 +4,9 @@ import Image from "next/image";
 import StatBlock from "@/components/StatBlock";
 import { BASE_URL } from "@/utils/constants";
 
+// Cache the page for 3 hours (10800 seconds) and revalidate after that
+export const revalidate = 10800;
+
 export default async function About() {
 	let stats = {
 		collection_count: 0,
@@ -13,7 +16,9 @@ export default async function About() {
 	};
 
 	try {
-		const response = await fetch(`${BASE_URL}/get_stats?`);
+		const response = await fetch(`${BASE_URL}/get_stats?`, {
+			next: { revalidate: 10800 }, // Revalidate the data every 3 hours
+		});
 		if (!response.ok) {
 			throw new Error(`HTTP error! Status: ${response.status}`);
 		}
@@ -41,12 +46,31 @@ export default async function About() {
 
 						<div className="row-span-3 text-justify leading-6">
 							<p>
-								At Ashesi, we are committed to fostering a culture of research, innovation, and knowledge sharing. The Ashesi Data Repository is a dedicated platform designed to store, manage, and provide access to valuable research data across various disciplines.
+								At Ashesi, we are committed to fostering a
+								culture of research, innovation, and knowledge
+								sharing. The Ashesi Data Repository is a
+								dedicated platform designed to store, manage,
+								and provide access to valuable research data
+								across various disciplines.
 							</p>
-							<br/>
-							<p>By centralizing datasets, research findings, and academic resources, the repository empowers students, faculty, and researchers to collaborate, explore new insights, and contribute to global knowledge. Through this initiative, we aim to enhance transparency, reproducibility, and the overall impact of research conducted within the Ashesi community and beyond.</p>
-							<br/>
-							<p>Join us in our mission to advance research excellence and drive meaningful change through data-driven discoveries.</p>
+							<br />
+							<p>
+								By centralizing datasets, research findings, and
+								academic resources, the repository empowers
+								students, faculty, and researchers to
+								collaborate, explore new insights, and
+								contribute to global knowledge. Through this
+								initiative, we aim to enhance transparency,
+								reproducibility, and the overall impact of
+								research conducted within the Ashesi community
+								and beyond.
+							</p>
+							<br />
+							<p>
+								Join us in our mission to advance research
+								excellence and drive meaningful change through
+								data-driven discoveries.
+							</p>
 						</div>
 					</div>
 
@@ -61,9 +85,18 @@ export default async function About() {
 						/>
 
 						<div className="grid grid-cols-2 gap-2 mb-8  justify-center items-center text-center h-16">
-							<StatBlock title={stats.collection_count} value="Collections" />
-							<StatBlock title={stats.author_count} value="Authors" />
-							<StatBlock title={stats.download_count} value="Downloads" />
+							<StatBlock
+								title={stats.collection_count}
+								value="Collections"
+							/>
+							<StatBlock
+								title={stats.author_count}
+								value="Authors"
+							/>
+							<StatBlock
+								title={stats.download_count}
+								value="Downloads"
+							/>
 							<StatBlock title={stats.view_count} value="Views" />
 						</div>
 					</div>
@@ -87,31 +120,36 @@ export default async function About() {
 							<span className="font-semibold">
 								Enhance Collaboration:
 							</span>{" "}
-							Connect with fellow researchers and contribute to collective knowledge across disciplines
+							Connect with fellow researchers and contribute to
+							collective knowledge across disciplines
 						</li>
 						<li className="mb-4">
 							<span className="font-semibold">
 								Ensure Data Integrity:
 							</span>{" "}
-							Securely store, manage, and preserve your research data for future reference and validation.
+							Securely store, manage, and preserve your research
+							data for future reference and validation.
 						</li>
 						<li className="mb-4">
 							<span className="font-semibold">
 								Increase Research Impact:
 							</span>{" "}
-							Improve visibility and accessibility, allowing your work to inform and inspire new discoveries.
+							Improve visibility and accessibility, allowing your
+							work to inform and inspire new discoveries.
 						</li>
 						<li className="mb-4">
 							<span className="font-semibold">
 								Support Open Science:
 							</span>{" "}
-							Promote transparency and knowledge-sharing by making research data
+							Promote transparency and knowledge-sharing by making
+							research data
 						</li>
 						<li className="mb-4">
 							<span className="font-semibold">
 								Facilitate Innovation:
 							</span>{" "}
-							Provide a foundation for data-driven insights and breakthroughs in diverse fields of study.
+							Provide a foundation for data-driven insights and
+							breakthroughs in diverse fields of study.
 						</li>
 					</ul>
 					<hr className="mb-10" />
