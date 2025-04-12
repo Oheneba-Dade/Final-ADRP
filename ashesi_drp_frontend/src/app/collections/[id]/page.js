45d1582422819e958b081a0d2226b90812d8b6d8
Oheneba Kwaku Addo Dade <ohenebadade@gmail.com>
2025-04-12 20:42:18 +0000

    fix: localStorage request on server side

--- ashesi_drp_frontend/src/app/collections/[id]/page.js
+++ ashesi_drp_frontend/src/app/collections/[id]/page.js
@@ -1,46 +1,62 @@
+// import "../../globals.css";
+//
+// import CollectionsDetails from "@/components/CollectionDetails";
+// import { BASE_URL } from "@/utils/constants";
+// import CollectionSidebar from "@/components/CollectionSidebar";
+//
+// export default async function CollectionPage({ params }) {
+// 	//The download count has a different endpoint, hence we fetch from both
+// 	const collectionId = params.id;
+// 	const jwt = localStorage.getItem("jwt");
+//
+// 	const collectionData = await fetch(
+// 		`${BASE_URL}/get_collection/?collection_id=${collectionId}`,
+// 		{
+// 			headers: {
+// 				Authorization: `Bearer ${jwt}`,
+// 			},
+// 		}
+// 	);
+// 	const downloadDataResponse = await fetch(
+// 		`${BASE_URL}/get_dataset/?collection_id=${collectionId}`,
+// 		{
+// 			headers: {
+// 				Authorization: `Bearer ${jwt}`,
+// 			},
+// 		}
+// 	);
+// 	const downloadData = await downloadDataResponse.json();
+// 	const initialCollection = await collectionData.json();
+//
+// 	return (
+// 		<div className="max-w-5xl mx-auto mt-20 px-6 py-10 grid grid-cols-1 md:grid-cols-4 gap-8">
+// 			{/* Left Column */}
+// 			<div className="md:col-span-3">
+// 				{/* Collection Detail */}
+// 				<CollectionsDetails initialCollection={initialCollection} />
+// 			</div>
+//
+// 			{/* Right Column*/}
+// 			<CollectionSidebar
+// 				initialCollection={initialCollection}
+// 				downloadData={downloadData}
+// 			/>
+// 		</div>
+// 	);
+// }
+
+
+
+
 import "../../globals.css";
+import CollectionClientLoader from "@/components/CollectionClientLoader"; // We'll create this
 
-import CollectionsDetails from "@/components/CollectionDetails";
-import { BASE_URL } from "@/utils/constants";
-import CollectionSidebar from "@/components/CollectionSidebar";
+export default function CollectionPage({ params }) {
+    const collectionId = params.id;
 
-export default async function CollectionPage({ params }) {
-	//The download count has a different endpoint, hence we fetch from both
-	const collectionId = params.id;
-	const jwt = localStorage.getItem("jwt");
-
-	const collectionData = await fetch(
-		`${BASE_URL}/get_collection/?collection_id=${collectionId}`,
-		{
-			headers: {
-				Authorization: `Bearer ${jwt}`,
-			},
-		}
-	);
-	const downloadDataResponse = await fetch(
-		`${BASE_URL}/get_dataset/?collection_id=${collectionId}`,
-		{
-			headers: {
-				Authorization: `Bearer ${jwt}`,
-			},
-		}
-	);
-	const downloadData = await downloadDataResponse.json();
-	const initialCollection = await collectionData.json();
-
-	return (
-		<div className="max-w-5xl mx-auto mt-20 px-6 py-10 grid grid-cols-1 md:grid-cols-4 gap-8">
-			{/* Left Column */}
-			<div className="md:col-span-3">
-				{/* Collection Detail */}
-				<CollectionsDetails initialCollection={initialCollection} />
-			</div>
-
-			{/* Right Column*/}
-			<CollectionSidebar
-				initialCollection={initialCollection}
-				downloadData={downloadData}
-			/>
-		</div>
-	);
+    return (
+        <div className="max-w-5xl mx-auto mt-20 px-6 py-10 grid grid-cols-1 md:grid-cols-4 gap-8">
+            <CollectionClientLoader collectionId={collectionId} />
+        </div>
+    );
 }
