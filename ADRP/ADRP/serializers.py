9a5613724e2ebdad8747932d47cd32ce93a86853
SKBlankson <114783019+SKBlankson@users.noreply.github.com>
2025-08-01 10:53:54 +0000

    Remove approve and rejection status from response

--- ADRP/ADRP/serializers.py
+++ ADRP/ADRP/serializers.py
@@ -36,8 +36,8 @@ class CollectionSerializer(serializers.ModelSerializer):
         model = Collection
         # fields = '__all__'
         fields = ['id', 'title', 'doi_link', 'keywords', 'abstract', 'instance_representation',
-                   'comment','date_of_publication', 'approval_status', 'approved_by' ,'approved_at', 
-                   'rejected_by' ,'rejected_at', 'view_count',  'authors']
+                   'comment','date_of_publication', 'approval_status','approved_at',
+                  'view_count',  'authors']
 
     def create(self, validated_data):
         # authors_data = validated_data.pop('authors', [])
