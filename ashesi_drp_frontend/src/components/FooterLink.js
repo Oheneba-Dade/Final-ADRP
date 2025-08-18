f5ddd9acacbddd52cb3eae17ba39cebffa76f30b
Reynolds Okyere Boakye <boakyeokyere96@gmail.com>
2025-08-18 19:58:49 +0000

    fix: policy link, page title and consent note

--- ashesi_drp_frontend/src/components/FooterLink.js
+++ ashesi_drp_frontend/src/components/FooterLink.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 import { LuArrowRight } from "react-icons/lu";
 
-const FooterLink = ({ title, link }) => {
+const FooterLink = ({ title, link, blank=false }) => {
 	return (
-		<Link href={link} className="block">
+		<Link href={link} className="block" target={blank ? "_blank" : "_self"} rel={blank ? "noopener noreferrer" : undefined}>
 		  <div className="bg-ashesi-gray w-full mb-2 p-2 hover:cursor-pointer hover:bg-gray-900 transition-colors duration-300 flex justify-between items-center">
 		    <span className="text-white hover:text-red-500 transition-colors duration-300 px-2">{title}</span>
 		    <LuArrowRight className="text-white" />
